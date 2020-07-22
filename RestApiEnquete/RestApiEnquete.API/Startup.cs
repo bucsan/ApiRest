6516bfff52dac57db36bff54ce1ed8a659f26f7a
Thiago Bucsan <thiago.bucsan@gmail.com>
2020-07-21 23:01:57 -0300

    Configurações
    - Autofac Service Provider Factory
    - Registro do ModuleIOC

--- RestApiEnquete/RestApiEnquete.API/Startup.cs
+++ RestApiEnquete/RestApiEnquete.API/Startup.cs
@@ -1,9 +1,14 @@
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RestApiEnquete.Infrastructure.CrossCutting.IOC;
+using RestApiEnquete.Infrastructure.Data;
 
 namespace RestApiEnquete.API
 {
@@ -19,11 +24,20 @@ namespace RestApiEnquete.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["SqlConnection:SqlConnectionString"];
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Enquete", Version = "V1" });
             });
+        }
+
+        // Configurando a injeção de dependência
+        public void ConfigurationContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ModuleIOC());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
